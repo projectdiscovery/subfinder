0f65797daf7e164afd4cdcb7f8e478c0b7247cfa
ice3man <nizamulrana@gmail.com>
2018-04-04 17:19:57 +0530

    Added coloring helper lib

---
+++ libsubfinder/helper/color.go
@@ -0,0 +1,20 @@
+// 
+// color.go : Contains color constants for printing
+// Written By : @ice3man (Nizamul Rana)
+// 
+// Distributed Under MIT License
+// Copyrights (C) 2018 Ice3man
+//
+
+package helper
+
+// use like this
+// fmt.Printf("[%sCRTSH%s] %s", r, rs, subdomain)
+
+var (
+	Red = "\033[31;1;4m"		// red color 
+	Cyan = "\033[36;1;4m"		// cyan color
+
+	Reset = "\033[0m"			// reset for default color
+
+)
