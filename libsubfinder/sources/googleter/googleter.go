4b5ac617b07bba37cb8d07b35af16919cc4a03c8
Ice3man543 <nizamulrana@gmail.com>
2018-06-26 14:05:57 +0530

    Fixed googleter engine with edge-case handling

--- libsubfinder/sources/googleter/googleter.go
+++ libsubfinder/sources/googleter/googleter.go
@@ -31,7 +31,7 @@ func makeRequest(token string, domain string, state *helper.State) (respBody []b
 	} else {
 		requestURI = "https://www.google.com/transparencyreport/api/v3/httpsreport/ct/certsearch/page?domain=" + url.QueryEscape(domain) + "&include_expired=true&include_subdomains=true&p=" + url.QueryEscape(token)
 	}
-	fmt.Printf("\n%s\n", requestURI)
+
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", requestURI, nil)
 	if err != nil {
@@ -64,22 +64,24 @@ func makeRequest(token string, domain string, state *helper.State) (respBody []b
 	return respBody, nil
 }
 
-func printSubdomains(match []string, state *helper.State) {
+func printSubdomains(match [][]string, state *helper.State) {
+	var finalSubdomain = ""
 	for _, subdomain := range match {
-		if !helper.SubdomainExists(subdomain, subdomains) == false {
-			if strings.Contains(subdomain, "*.") {
-				subdomain = strings.Split(subdomain, "*.")[1]
+		if !helper.SubdomainExists(subdomain[0], subdomains) {
+			finalSubdomain = subdomain[0]
+			if strings.Contains(subdomain[0], "*.") {
+				finalSubdomain = strings.Split(subdomain[0], "*.")[1]
 			}
 
 			if state.Verbose == true {
 				if state.Color == true {
-					fmt.Printf("\n[%sGoogleTER%s] %s", helper.Red, helper.Reset, subdomain)
+					fmt.Printf("\n[%sGoogleTER%s] %s", helper.Red, helper.Reset, finalSubdomain)
 				} else {
-					fmt.Printf("\n[GoogleTER] %s", subdomain)
+					fmt.Printf("\n[GoogleTER] %s", finalSubdomain)
 				}
 			}
 
-			subdomains = append(subdomains, subdomain)
+			subdomains = append(subdomains, finalSubdomain)
 		}
 	}
 
@@ -100,17 +102,34 @@ func Query(args ...interface{}) interface{} {
 		return subdomains
 	}
 
+	var Token string
+
 	metaRegex := regexp.MustCompile(`\[null,"(.*)",null,(.*),(.*)]`)
 	matches := metaRegex.FindStringSubmatch(string(respBody))
 	if len(matches) <= 1 {
 		return subdomains
 	}
 
-	subdomaianRegex := regexp.MustCompile(`,"*.` + domain + `",`)
-	match := subdomaianRegex.FindStringSubmatch(string(respBody))
+	subdomainRegex := regexp.MustCompile(`([A-Za-z0-9]+\.)+` + domain)
+	match := subdomainRegex.FindAllStringSubmatch(string(respBody), -1)
 	printSubdomains(match, state)
 
-	Token := matches[1]
+	// In some weird cases, googleter returns specifc hashes embedded in the results.
+	// We fall back to this regex in order to to find that token
+	cryptoSHA2 := regexp.MustCompile(`\[null,"B05DFBBD58ECDB8D18100E6AA4DA0C64AECA148D41971942A8E2068375063759",null,(.*),(.*)]`)
+	mcryptoSHA2 := cryptoSHA2.FindStringSubmatch(string(respBody))
+	if len(mcryptoSHA2) >= 1 {
+		Token = mcryptoSHA2[0]
+	} else {
+		cryptoSHA2CBC := regexp.MustCompile(`\[null,"A0315234520886D17581376D876B44FE0FADAD26CA47A9A0A1F4BA9BD8735947",null,(.*),(.*)]`)
+		mcryptoSHA2CBC := cryptoSHA2CBC.FindStringSubmatch(string(respBody))
+		if len(mcryptoSHA2CBC) >= 1 {
+			Token = mcryptoSHA2[0]
+		} else {
+			Token = matches[1]
+		}
+	}
+
 	MaxPages, _ := strconv.Atoi(matches[3])
 	for i := 1; i <= MaxPages; i++ {
 		respBody, err = makeRequest(Token, domain, state)
@@ -121,7 +140,7 @@ func Query(args ...interface{}) interface{} {
 			return subdomains
 		}
 
-		match := subdomaianRegex.FindStringSubmatch(string(respBody))
+		match := subdomainRegex.FindAllStringSubmatch(string(respBody), -1)
 		printSubdomains(match, state)
 
 		metaRegex2 := regexp.MustCompile(`\["(.*)",".*",null,(.*),(.*)]`)
