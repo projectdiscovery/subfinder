7aec8084523c9acafcb7dc033ec09d8bfc1ab455
Nizamul Rana <nizamulrana@gmail.com>
2018-07-22 14:05:40 +0530

    Updated resolvers to include google+cloudflare+quad9+level3+opendns

--- subf/subfinder.go
+++ subf/subfinder.go
@@ -156,8 +156,15 @@ func (s *Subfinder) setCommonResolver() {
 	}
 
 	s.State.LoadResolver = append(s.State.LoadResolver, "1.1.1.1")
+	s.State.LoadResolver = append(s.State.LoadResolver, "1.0.0.1")
 	s.State.LoadResolver = append(s.State.LoadResolver, "8.8.8.8")
 	s.State.LoadResolver = append(s.State.LoadResolver, "8.8.4.4")
+	s.State.LoadResolver = append(s.State.LoadResolver, "9.9.9.9")
+	s.State.LoadResolver = append(s.State.LoadResolver, "9.9.9.10")
+	s.State.LoadResolver = append(s.State.LoadResolver, "209.244.0.3")
+	s.State.LoadResolver = append(s.State.LoadResolver, "209.244.0.4")
+	s.State.LoadResolver = append(s.State.LoadResolver, "208.67.222.222")
+	s.State.LoadResolver = append(s.State.LoadResolver, "208.67.220.220")
 }
 
 func (s *Subfinder) setOutput() {
