527e05e7662fcc47de2d0dfbbf2d6e759d42f6ab
Ice3man543 <nizamulrana@gmail.com>
2019-12-03 18:00:17 +0530

    Added cmd package

---
+++ cmd/subfinder/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/subfinder/subfinder/pkg/runner"
+
+func main() {
+	// Parse the command line flags and read config files
+	options := runner.ParseOptions()
+
+	_ = options
+}
