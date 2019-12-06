a46182925ba38d37135414de92cfeb4a9cac753c
Tevin Zhang <mail2tevin@gmail.com>
2019-12-06 19:00:22 +0800

    Add a temporary interface of session for testing

--- pkg/subscraping/sources/bufferover/bufferover.go
+++ pkg/subscraping/sources/bufferover/bufferover.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"regexp"
 
 	"github.com/projectdiscovery/subfinder/pkg/subscraping"
 )
@@ -13,7 +15,7 @@ import (
 type Source struct{}
 
 // Run function returns all subdomains found with the service
-func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Session) <-chan subscraping.Result {
+func (s *Source) Run(ctx context.Context, domain string, session Session) <-chan subscraping.Result {
 	results := make(chan subscraping.Result)
 
 	go func() {
@@ -27,7 +29,7 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 	return results
 }
 
-func (s *Source) getData(URL string, session *subscraping.Session, results chan subscraping.Result) {
+func (s *Source) getData(URL string, session Session, results chan subscraping.Result) {
 	resp, err := session.NormalGet(URL)
 	if err != nil {
 		results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
@@ -45,7 +47,7 @@ func (s *Source) getData(URL string, session *subscraping.Session, results chan 
 
 	src := string(body)
 
-	for _, subdomain := range session.Extractor.FindAllString(src, -1) {
+	for _, subdomain := range session.GetExtractor().FindAllString(src, -1) {
 		results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: subdomain}
 	}
 	return
@@ -54,4 +56,21 @@ func (s *Source) getData(URL string, session *subscraping.Session, results chan 
 // Name returns the name of the source
 func (s *Source) Name() string {
 	return "bufferover"
+}
+
+// Session is an interface of *subscraping.Session, created temporarily to make this package testable.
+// TODO: A full version of this interface may be placed in upper level.
+type Session interface {
+	NormalGetter
+	Extractor
+}
+
+// NormalGetter represents the ability to make an HTTP(s) GET request.
+type NormalGetter interface {
+	NormalGet(url string) (*http.Response, error)
+}
+
+// Extractor represents the ability to get an regex.
+type Extractor interface {
+	GetExtractor() *regexp.Regexp
 }
