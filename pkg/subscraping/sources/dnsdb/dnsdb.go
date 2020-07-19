d342973eba644857668e649192ffc3dd92771bfc
Víctor Zamanillo <bellzebu@hotmail.com>
2020-07-19 17:57:00 +0200

    Do not run dnsdb source if keys are not specified

--- pkg/subscraping/sources/dnsdb/dnsdb.go
+++ pkg/subscraping/sources/dnsdb/dnsdb.go
@@ -22,47 +22,55 @@ type Source struct{}
 // Run function returns all subdomains found with the service
 func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Session) <-chan subscraping.Result {
 	results := make(chan subscraping.Result)
-	headers := map[string]string{
-		"X-API-KEY":    session.Keys.DNSDB,
-		"Accept":       "application/json",
-		"Content-Type": "application/json",
-	}
-	go func() {
-		resp, err := session.Get(ctx, fmt.Sprintf("https://api.dnsdb.info/lookup/rrset/name/*.%s?limit=1000000000000", domain), "", headers)
-		if err != nil {
-			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
-			close(results)
-			return
+
+	if session.Keys.DNSDB == "" {
+		close(results)
+	} else {
+		headers := map[string]string{
+			"X-API-KEY":    session.Keys.DNSDB,
+			"Accept":       "application/json",
+			"Content-Type": "application/json",
 		}
-		// Check status code
-		if resp.StatusCode != 200 {
-			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: fmt.Errorf("invalid status code received: %d", resp.StatusCode)}
-			io.Copy(ioutil.Discard, resp.Body)
-			resp.Body.Close()
-			close(results)
-			return
-		}
-		defer resp.Body.Close()
-		// Get the response body
-		scanner := bufio.NewScanner(resp.Body)
-		for scanner.Scan() {
-			line := scanner.Text()
-			if line == "" {
-				continue
-			}
-			out := &dnsdbResponse{}
-			err := json.Unmarshal([]byte(line), out)
+
+		go func() {
+			resp, err := session.Get(ctx, fmt.Sprintf("https://api.dnsdb.info/lookup/rrset/name/*.%s?limit=1000000000000", domain), "", headers)
 			if err != nil {
 				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+				close(results)
+				return
+			}
+
+			// Check status code
+			if resp.StatusCode != 200 {
+				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: fmt.Errorf("invalid status code received: %d", resp.StatusCode)}
+				io.Copy(ioutil.Discard, resp.Body)
 				resp.Body.Close()
 				close(results)
 				return
 			}
-			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: strings.TrimSuffix(out.Name, ".")}
-			out = nil
-		}
-		close(results)
-	}()
+
+			defer resp.Body.Close()
+			// Get the response body
+			scanner := bufio.NewScanner(resp.Body)
+			for scanner.Scan() {
+				line := scanner.Text()
+				if line == "" {
+					continue
+				}
+				out := &dnsdbResponse{}
+				err := json.Unmarshal([]byte(line), out)
+				if err != nil {
+					results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+					resp.Body.Close()
+					close(results)
+					return
+				}
+				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: strings.TrimSuffix(out.Name, ".")}
+				out = nil
+			}
+			close(results)
+		}()
+	}
 	return results
 }
 
