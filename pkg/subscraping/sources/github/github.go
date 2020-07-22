75c3682b6e822a8e4fc1c1f01838f96b1b7a5d9c
Víctor Zamanillo <bellzebu@hotmail.com>
2020-07-22 15:53:59 +0200

    Fixed S1002: should omit comparison to bool constant (gosimple)

--- pkg/subscraping/sources/github/github.go
+++ pkg/subscraping/sources/github/github.go
@@ -175,8 +175,8 @@ func normalizeContent(content string) string {
 func unique(arr []string) []string {
 	occurred := map[string]bool{}
 	result := []string{}
-	for e := range arr {
-		if occurred[arr[e]] != true {
+	for e := range arr {		
+		if !occurred[arr[e]] {
 			occurred[arr[e]] = true
 			result = append(result, arr[e])
 		}
