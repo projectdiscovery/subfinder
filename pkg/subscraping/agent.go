3eb417db16156edc13ce48f2841253f137ef63d0
Víctor Zamanillo <bellzebu@hotmail.com>
2020-07-22 15:25:02 +0200

    Fixed S1031: unnecessary nil check around range (gosimple)

--- pkg/subscraping/agent.go
+++ pkg/subscraping/agent.go
@@ -66,10 +66,8 @@ func (s *Session) Get(ctx context.Context, url string, cookies string, headers m
 		req.Header.Set("Cookie", cookies)
 	}
 
-	if headers != nil {
-		for key, value := range headers {
-			req.Header.Set(key, value)
-		}
+	for key, value := range headers {
+		req.Header.Set(key, value)
 	}
 
 	return httpRequestWrapper(s.Client, req)
