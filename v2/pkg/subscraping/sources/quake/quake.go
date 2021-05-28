ab5374dac1072704bc52e9bd5829a65ed55a210a
sandeep <8293321+ehsandeep@users.noreply.github.com>
2021-05-28 16:58:35 +0530

    syntax improvements

--- v2/pkg/subscraping/sources/quake/quake.go
+++ v2/pkg/subscraping/sources/quake/quake.go
@@ -17,14 +17,14 @@ type quakeResults struct {
 		Service struct {
 			HTTP struct {
 				//Title           string `json:"title"`
-				Host            string `json:"host"`
-				StatusCode      int    `json:"status_code"`
+				Host       string `json:"host"`
+				StatusCode int    `json:"status_code"`
 				//ResponseHeaders string `json:"response_headers"`
 			} `json:"http"`
 		}
 	}
 	Meta struct {
-		Pagination struct{
+		Pagination struct {
 			Total int `json:"total"`
 		} `json:"pagination"`
 	} `json:"meta"`
@@ -39,13 +39,13 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 	go func() {
 		defer close(results)
 
-		if session.Keys.Quake == ""  {
+		if session.Keys.Quake == "" {
 			return
 		}
 
 		// quake api doc https://quake.360.cn/quake/#/help
 		var requestBody = []byte(`{"query":"domain: *.` + domain + `", "start":0, "size":1000}`)
-		resp, err := session.Post(ctx, "https://quake.360.cn/api/v3/search/quake_service", "", map[string]string{"Content-Type": "application/json", "X-QuakeToken": session.Keys.Quake},bytes.NewReader(requestBody))
+		resp, err := session.Post(ctx, "https://quake.360.cn/api/v3/search/quake_service", "", map[string]string{"Content-Type": "application/json", "X-QuakeToken": session.Keys.Quake}, bytes.NewReader(requestBody))
 		if err != nil {
 			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
 			session.DiscardHTTPResponse(resp)
@@ -69,7 +69,7 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 		if response.Meta.Pagination.Total > 0 {
 			for _, quakeDomain := range response.Data {
 				subdomain := quakeDomain.Service.HTTP.Host
-				if strings.ContainsAny(subdomain, "暂无权限")  {
+				if strings.ContainsAny(subdomain, "暂无权限") {
 					subdomain = ""
 				}
 				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: subdomain}
@@ -84,4 +84,3 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 func (s *Source) Name() string {
 	return "quake"
 }
-
