60223b4507f5217b9987da795aeb285ff6902e38
LuitelSamikshya <luitelsamikshya1@gmail.com>
2022-08-04 08:57:23 -0500

    added more test cases

--- v2/pkg/runner/enumerate_test.go
+++ v2/pkg/runner/enumerate_test.go
@@ -6,40 +6,117 @@ import (
 	"testing"
 )
 
-func TestFilterAndMatchSubdomainForMatch(t *testing.T) {
+func TestFilterAndMatchSubdomain(t *testing.T) {
 	options := &Options{}
 	options.Domain = []string{"example.com"}
 	options.Threads = 10
 	options.Timeout = 10
 	options.Output = os.Stdout
-	options.Match = []string{"*.example.com"}
-	err := options.validateOptions()
-	if err != nil {
-		t.Fatalf("Expected nil got %v while validation\n", err)
-	}
-	runner, err := NewRunner(options)
-	if err != nil {
-		t.Fatalf("Expected nil got %v while creating runner\n", err)
-	}
-	match := runner.filterAndMatchSubdomain("ns.example.com")
-	require.True(t, match, "Expecting a boolean value ")
-}
-
-func TestFilterAndMatchSubdomainForFilter(t *testing.T) {
-	options := &Options{}
-	options.Domain = []string{"example.com"}
-	options.Threads = 10
-	options.Timeout = 10
-	options.Output = os.Stdout
-	options.Filter = []string{"*.example.com"}
-	err := options.validateOptions()
-	if err != nil {
-		t.Fatalf("Expected nil got %v while validation\n", err)
-	}
-	runner, err := NewRunner(options)
-	if err != nil {
-		t.Fatalf("Expected nil got %v while creating runner\n", err)
-	}
-	match := runner.filterAndMatchSubdomain("ns.example.com")
-	require.False(t, match, "Expecting a False value ")
+	t.Run("Literal Match", func(t *testing.T) {
+		options.Match = []string{"req.example.com"}
+		err := options.validateOptions()
+		if err != nil {
+			t.Fatalf("Expected nil got %v while validation\n", err)
+		}
+		runner, err := NewRunner(options)
+		if err != nil {
+			t.Fatalf("Expected nil got %v while creating runner\n", err)
+		}
+		match := runner.filterAndMatchSubdomain("req.example.com")
+		require.True(t, match, "Expecting a boolean True value ")
+	})
+	t.Run("Multiple Wildcards Match", func(t *testing.T) {
+		options.Match = []string{"*.ns.*.com"}
+		err := options.validateOptions()
+		if err != nil {
+			t.Fatalf("Expected nil got %v while validation\n", err)
+		}
+		runner, err := NewRunner(options)
+		if err != nil {
+			t.Fatalf("Expected nil got %v while creating runner\n", err)
+		}
+		subdomain := []string{"a.ns.example.com", "b.ns.hackerone.com"}
+		for _, sub := range subdomain {
+			match := runner.filterAndMatchSubdomain(sub)
+			require.True(t, match, "Expecting a boolean True value ")
+		}
+	})
+	t.Run("Sequential Match", func(t *testing.T) {
+		options.Match = []string{"*.ns.example.com", "*.hackerone.com"}
+		err := options.validateOptions()
+		if err != nil {
+			t.Fatalf("Expected nil got %v while validation\n", err)
+		}
+		runner, err := NewRunner(options)
+		if err != nil {
+			t.Fatalf("Expected nil got %v while creating runner\n", err)
+		}
+		subdomain := []string{"a.ns.example.com", "b.hackerone.com"}
+		for _, sub := range subdomain {
+			match := runner.filterAndMatchSubdomain(sub)
+			require.True(t, match, "Expecting a boolean True value ")
+		}
+	})
+	t.Run("Literal Filter", func(t *testing.T) {
+		options.Filter = []string{"req.example.com"}
+		err := options.validateOptions()
+		if err != nil {
+			t.Fatalf("Expected nil got %v while validation\n", err)
+		}
+		runner, err := NewRunner(options)
+		if err != nil {
+			t.Fatalf("Expected nil got %v while creating runner\n", err)
+		}
+		match := runner.filterAndMatchSubdomain("req.example.com")
+		require.False(t, match, "Expecting a boolean False value ")
+	})
+	t.Run("Multiple Wildcards Filter", func(t *testing.T) {
+		options.Filter = []string{"*.ns.*.com"}
+		err := options.validateOptions()
+		if err != nil {
+			t.Fatalf("Expected nil got %v while validation\n", err)
+		}
+		runner, err := NewRunner(options)
+		if err != nil {
+			t.Fatalf("Expected nil got %v while creating runner\n", err)
+		}
+		subdomain := []string{"a.ns.example.com", "b.ns.hackerone.com"}
+		for _, sub := range subdomain {
+			match := runner.filterAndMatchSubdomain(sub)
+			require.False(t, match, "Expecting a boolean False value ")
+		}
+	})
+	t.Run("Sequential Filter", func(t *testing.T) {
+		options.Filter = []string{"*.ns.example.com", "*.hackerone.com"}
+		err := options.validateOptions()
+		if err != nil {
+			t.Fatalf("Expected nil got %v while validation\n", err)
+		}
+		runner, err := NewRunner(options)
+		if err != nil {
+			t.Fatalf("Expected nil got %v while creating runner\n", err)
+		}
+		subdomain := []string{"a.ns.example.com", "b.hackerone.com"}
+		for _, sub := range subdomain {
+			match := runner.filterAndMatchSubdomain(sub)
+			require.False(t, match, "Expecting a boolean False value ")
+		}
+	})
+	t.Run("Filter and Match", func(t *testing.T) {
+		options.Filter = []string{"example.com"}
+		options.Match = []string{"hackerone.com"}
+		err := options.validateOptions()
+		if err != nil {
+			t.Fatalf("Expected nil got %v while validation\n", err)
+		}
+		runner, err := NewRunner(options)
+		if err != nil {
+			t.Fatalf("Expected nil got %v while creating runner\n", err)
+		}
+		subdomain := []string{"example.com", "example.com"}
+		for _, sub := range subdomain {
+			match := runner.filterAndMatchSubdomain(sub)
+			require.False(t, match, "Expecting a boolean False value ")
+		}
+	})
 }
