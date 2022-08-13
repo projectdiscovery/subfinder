5e281cb64c0905644146053926357b1ee542fe71
Siddharth Balyan <balyan.sid@gmail.com>
2022-08-13 19:46:34 +0200

    BeVigil Source Added

--- v2/pkg/passive/sources.go
+++ v2/pkg/passive/sources.go
@@ -8,6 +8,7 @@ import (
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/alienvault"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/anubis"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/archiveis"
+	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/bevigil"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/binaryedge"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/bufferover"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/c99"
@@ -46,6 +47,7 @@ var AllSources = [...]subscraping.Source{
 	&alienvault.Source{},
 	&anubis.Source{},
 	&archiveis.Source{},
+	&bevigil.Source{},
 	&binaryedge.Source{},
 	&bufferover.Source{},
 	&c99.Source{},
--- v2/pkg/passive/sources_test.go
+++ v2/pkg/passive/sources_test.go
@@ -14,6 +14,7 @@ var (
 		"alienvault",
 		"anubis",
 		"archiveis",
+		"bevigil",
 		"binaryedge",
 		"bufferover",
 		"c99",
@@ -51,6 +52,7 @@ var (
 	expectedDefaultSources = []string{
 		"alienvault",
 		"anubis",
+		"bevigil",
 		"bufferover",
 		"c99",
 		"certspotter",
