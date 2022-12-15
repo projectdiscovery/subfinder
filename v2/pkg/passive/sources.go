27d408785a9f3dd6ccf6b3487f281a2c7d76d296
Víctor <bellzebu@hotmail.com>
2022-12-15 12:16:15 +0100

    Gitlab search through the Gitlab Search API (#694)
    * Gitlab search through the Gitlab Search API
    
    * Sorted AllSources
    
    * Error handling for network related errors (context deadline)
    Concurrent item processing
    Hardcoded delay before parsing every 100 item block
    
    * Wait per results block
    
    * digitorus source search (#696)
    
    * digitorus source
    
    * Sorted AllSources
    
    * Fixed TestSourceFiltering after merging dev branch
    
    Co-authored-by: Víctor Zamanillo <victor.zamanillo@cifraeducacion.com>
    Co-authored-by: sandeep <8293321+ehsandeep@users.noreply.github.com>

--- v2/pkg/passive/sources.go
+++ v2/pkg/passive/sources.go
@@ -27,6 +27,7 @@ import (
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/fofa"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/fullhunt"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/github"
+	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/gitlab"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/hackertarget"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/hunter"
 	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/intelx"
@@ -66,6 +67,7 @@ var AllSources = [...]subscraping.Source{
 	&fofa.Source{},
 	&fullhunt.Source{},
 	&github.Source{},
+	&gitlab.Source{},
 	&hackertarget.Source{},
 	&hunter.Source{},
 	&intelx.Source{},
