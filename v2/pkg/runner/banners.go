9250365bcb8126678f94b29bdea2929a8acc6eb8
sandeep <8293321+ehsandeep@users.noreply.github.com>
2023-02-22 20:08:45 +0530

    workflow updates

--- v2/pkg/runner/banners.go
+++ v2/pkg/runner/banners.go
@@ -9,18 +9,14 @@ const banner = `
    _______  __/ /_  / __(_)___  ____/ /__  _____
   / ___/ / / / __ \/ /_/ / __ \/ __  / _ \/ ___/
  (__  ) /_/ / /_/ / __/ / / / / /_/ /  __/ /    
-/____/\__,_/_.___/_/ /_/_/ /_/\__,_/\___/_/ v2.5.5
+/____/\__,_/_.___/_/ /_/_/ /_/\__,_/\___/_/ v2.5.6
 `
 
 // Version is the current version of subfinder
-const Version = `v2.5.5`
+const Version = `v2.5.6`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
 	gologger.Print().Msgf("%s\n", banner)
 	gologger.Print().Msgf("\t\tprojectdiscovery.io\n\n")
-
-	gologger.Print().Msgf("Use with caution. You are responsible for your actions\n")
-	gologger.Print().Msgf("Developers assume no liability and are not responsible for any misuse or damage.\n")
-	gologger.Print().Msgf("By using subfinder, you also agree to the terms of the APIs used.\n\n")
 }
