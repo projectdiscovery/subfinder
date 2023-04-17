5d49478b97e237d87ca02ecb0c7b7e835210d397
Gregory Boddin <gboddin@users.noreply.github.com>
2023-04-17 10:15:08 +0200

    new source leakix (#743)  (#793)
    * new source leakix (#743)
    
    * moved leakix import
    
    * leakix: using fmt.Errorf

---
+++ v2/pkg/subscraping/sources/leakix/leakix.go
@@ -0,0 +1,106 @@
+// Package leakix logic
+package leakix
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping"
+)
+
+// Source is the passive scraping agent
+type Source struct {
+	apiKeys   []string
+	timeTaken time.Duration
+	errors    int
+	results   int
+	skipped   bool
+}
+
+// Run function returns all subdomains found with the service
+func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Session) <-chan subscraping.Result {
+	results := make(chan subscraping.Result)
+	s.errors = 0
+	s.results = 0
+
+	go func() {
+		defer func(startTime time.Time) {
+			s.timeTaken = time.Since(startTime)
+			close(results)
+		}(time.Now())
+		// Default headers
+		headers := map[string]string{
+			"accept": "application/json",
+		}
+		// Pick an API key
+		randomApiKey := subscraping.PickRandom(s.apiKeys, s.Name())
+		if randomApiKey != "" {
+			headers["api-key"] = randomApiKey
+		}
+		// Request
+		resp, err := session.Get(ctx, "https://leakix.net/api/subdomains/"+domain, "", headers)
+		if err != nil {
+			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+			s.errors++
+			return
+		} else if resp.StatusCode != 200 {
+			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: fmt.Errorf("request failed with status %d", resp.StatusCode)}
+			s.errors++
+			return
+		}
+		// Parse and return results
+		var subdomains []subResponse
+		decoder := json.NewDecoder(resp.Body)
+		err = decoder.Decode(&subdomains)
+		if err != nil {
+			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+			s.errors++
+			return
+		}
+		for _, result := range subdomains {
+			results <- subscraping.Result{
+				Source: s.Name(), Type: subscraping.Subdomain, Value: result.Subdomain,
+			}
+			s.results++
+		}
+	}()
+	return results
+}
+
+// Name returns the name of the source
+func (s *Source) Name() string {
+	return "leakix"
+}
+
+func (s *Source) IsDefault() bool {
+	return true
+}
+
+func (s *Source) HasRecursiveSupport() bool {
+	return true
+}
+
+func (s *Source) NeedsKey() bool {
+	return true
+}
+
+func (s *Source) AddApiKeys(keys []string) {
+	s.apiKeys = keys
+}
+
+func (s *Source) Statistics() subscraping.Statistics {
+	return subscraping.Statistics{
+		Errors:    s.errors,
+		Results:   s.results,
+		TimeTaken: s.timeTaken,
+		Skipped:   s.skipped,
+	}
+}
+
+type subResponse struct {
+	Subdomain   string    `json:"subdomain"`
+	DistinctIps int       `json:"distinct_ips"`
+	LastSeen    time.Time `json:"last_seen"`
+}
