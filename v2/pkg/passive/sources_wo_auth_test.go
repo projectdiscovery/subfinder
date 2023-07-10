f92cbc0953a5cb15015ae0878a49c12c31b28dc6
Mzack9999 <mzack9999@protonmail.com>
2023-07-10 16:27:16 +0200

    flagging hackertarget as ignored

--- v2/pkg/passive/sources_wo_auth_test.go
+++ v2/pkg/passive/sources_wo_auth_test.go
@@ -20,9 +20,10 @@ import (
 
 func TestSourcesWithoutKeys(t *testing.T) {
 	ignoredSources := []string{
-		"commoncrawl", // commoncrawl is under resourced and will likely time-out so step over it for this test https://groups.google.com/u/2/g/common-crawl/c/3QmQjFA_3y4/m/vTbhGqIBBQAJ
-		"riddler",     // Fails with 403: There might be too much traffic or a configuration error
-		"crtsh",       // Fails in GH Action (possibly IP-based ban) causing a timeout.
+		"commoncrawl",  // commoncrawl is under resourced and will likely time-out so step over it for this test https://groups.google.com/u/2/g/common-crawl/c/3QmQjFA_3y4/m/vTbhGqIBBQAJ
+		"riddler",      // Fails with 403: There might be too much traffic or a configuration error
+		"crtsh",        // Fails in GH Action (possibly IP-based ban) causing a timeout.
+		"hackertarget", // Fails in GH Action (possibly IP-based ban) but works locally
 	}
 
 	domain := "hackerone.com"
