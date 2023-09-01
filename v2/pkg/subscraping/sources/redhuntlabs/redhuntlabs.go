4ab568637ae3ea5601514901e7cdb7a8e8f2fac7
umair-rhl <umair.nehri@redhuntlabs.com>
2023-09-01 17:25:13 +0530

    improved error handling

--- v2/pkg/subscraping/sources/redhuntlabs/redhuntlabs.go
+++ v2/pkg/subscraping/sources/redhuntlabs/redhuntlabs.go
@@ -3,6 +3,8 @@ package redhuntlabs
 
 import (
 	"context"
+	"errors"
+	"strconv"
 	"strings"
 	"time"
 
@@ -12,7 +14,14 @@ import (
 )
 
 type Response struct {
-	Subdomains []string `json:"subdomains"`
+	Subdomains []string         `json:"subdomains"`
+	Metadata   ResponseMetadata `json:"metadata"`
+}
+
+type ResponseMetadata struct {
+	ResultCount int `json:"result_count"`
+	PageSize    int `json:"page_size"`
+	PageNumber  int `json:"page_number"`
 }
 
 type Source struct {
@@ -27,7 +36,7 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 	results := make(chan subscraping.Result)
 	s.errors = 0
 	s.results = 0
-
+	pageSize := 1000
 	go func() {
 		defer func(startTime time.Time) {
 			s.timeTaken = time.Since(startTime)
@@ -41,17 +50,17 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 		}
 
 		creds := strings.Split(randomCred, ":")
-		getUrl := creds[0] + ":" + creds[1] + "?domain=" + domain
+		baseUrl := creds[0] + ":" + creds[1]
+		getUrl := baseUrl + "?domain=" + domain + "&page=1&page_size=" + strconv.Itoa(pageSize)
 		resp, err := session.Get(ctx, getUrl, "", map[string]string{
 			"X-BLOBR-KEY": creds[2], "User-Agent": "subfinder",
 		})
 		if err != nil {
 			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: errors.New("if you get a 'limit has been reached' error, head over to https://devportal.redhuntlabs.com/ for upgrading your subscription")}
 			session.DiscardHTTPResponse(resp)
 			return
 		}
-
-		var subdomains []string
 		var response Response
 		err = jsoniter.NewDecoder(resp.Body).Decode(&response)
 		if err != nil {
@@ -61,12 +70,43 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 		}
 
 		resp.Body.Close()
-		if len(response.Subdomains) > 0 {
-			subdomains = response.Subdomains
-		}
+		if response.Metadata.ResultCount > pageSize {
+			totalPages := (response.Metadata.ResultCount + pageSize - 1) / pageSize
+			for page := 1; page <= totalPages; page++ {
+				getUrl = baseUrl + "?domain=" + domain + "&page=" + strconv.Itoa(page) + "&page_size=" + strconv.Itoa(pageSize)
+				resp, err := session.Get(ctx, getUrl, "", map[string]string{
+					"X-BLOBR-KEY": creds[2], "User-Agent": "subfinder",
+				})
+				if err != nil {
+					results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+					results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: errors.New("if you get a 'limit has been reached' error, head over to https://devportal.redhuntlabs.com/ for upgrading your subscription")}
+					session.DiscardHTTPResponse(resp)
+					continue
+				}
 
-		for _, subdomain := range subdomains {
-			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: subdomain}
+				var subdomains []string
+				err = jsoniter.NewDecoder(resp.Body).Decode(&response)
+				if err != nil {
+					results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+					resp.Body.Close()
+					continue
+				}
+
+				resp.Body.Close()
+				if len(response.Subdomains) > 0 {
+					subdomains = response.Subdomains
+				}
+
+				for _, subdomain := range subdomains {
+					results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: subdomain}
+				}
+			}
+		} else {
+			if len(response.Subdomains) > 0 {
+				for _, subdomain := range response.Subdomains {
+					results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: subdomain}
+				}
+			}
 		}
 
 	}()
