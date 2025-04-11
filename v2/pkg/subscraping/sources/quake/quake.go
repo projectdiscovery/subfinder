1464e5ad83fa5789a78c7727b3fb7413b5b8a891
ChenDark <53772533+YouChenJun@users.noreply.github.com>
2025-04-11 09:28:25 +0800

    fix(subscraping):  Fix the paging logic of Hunter and Quake sources
    -Modify the pagination calculation method of the Hunter source and adjust the number of pages from 1000 to 100
    -Improve the paging logic of the Quake source, add the acquisition and processing of multi page data
    -Optimize the request body format of the shake source and add the 'latest' parameter

--- v2/pkg/subscraping/sources/quake/quake.go
+++ v2/pkg/subscraping/sources/quake/quake.go
@@ -56,9 +56,10 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 			s.skipped = true
 			return
 		}
-
+		//翻页设计
+		var pages = 1
 		// quake api doc https://quake.360.cn/quake/#/help
-		var requestBody = []byte(fmt.Sprintf(`{"query":"domain: %s", "include":["service.http.host"], "latest": true, "start":0, "size":500}`, domain))
+		var requestBody = []byte(fmt.Sprintf(`{"query":"domain: %s", "include":["service.http.host"], "latest": true, "start":0, "size":500 ,"latest":true}`, domain))
 		resp, err := session.Post(ctx, "https://quake.360.net/api/v3/search/quake_service", "", map[string]string{
 			"Content-Type": "application/json", "X-QuakeToken": randomApiKey,
 		}, bytes.NewReader(requestBody))
@@ -95,6 +96,52 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 				}
 				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: subdomain}
 				s.results++
+			}
+		}
+
+		// get all pages
+		pages = int(response.Meta.Pagination.Total/500) + 1
+		for currentPage := 2; currentPage <= pages; currentPage++ {
+			//Preform page turning operaation
+			var start = (currentPage - 1) * 500
+			var requestBody = []byte(fmt.Sprintf(`{"query":"domain: %s", "include":["service.http.host"], "latest": true, "start":%d, "size":500 ,"latest":true}`, domain, start))
+			resp, err := session.Post(ctx, "https://quake.360.net/api/v3/search/quake_service", "", map[string]string{
+				"Content-Type": "application/json", "X-QuakeToken": randomApiKey,
+			}, bytes.NewReader(requestBody))
+			if err != nil {
+				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+				s.errors++
+				session.DiscardHTTPResponse(resp)
+				return
+			}
+
+			var response quakeResults
+			err = jsoniter.NewDecoder(resp.Body).Decode(&response)
+			if err != nil {
+				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+				s.errors++
+				resp.Body.Close()
+				return
+			}
+			resp.Body.Close()
+
+			if response.Code != 0 {
+				results <- subscraping.Result{
+					Source: s.Name(), Type: subscraping.Error, Error: fmt.Errorf("%s", response.Message),
+				}
+				s.errors++
+				return
+			}
+
+			if response.Meta.Pagination.Total > 0 {
+				for _, quakeDomain := range response.Data {
+					subdomain := quakeDomain.Service.HTTP.Host
+					if strings.ContainsAny(subdomain, "暂无权限") {
+						subdomain = ""
+					}
+					results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: subdomain}
+					s.results++
+				}
 			}
 		}
 	}()
