42c193dd5f68aefe48bcda469abd4478d51ee3ac
xsrpatric <srpatric.ti@gmail.com>
2026-02-08 18:39:34 -0300

    perf: Aumenta o número padrão de threads para 20

--- pkg/runner/options.go
+++ pkg/runner/options.go
@@ -103,7 +103,7 @@ func ParseOptions() *Options {
 
 	flagSet.CreateGroup("rate-limit", "Rate-limit",
 		flagSet.IntVarP(&options.RateLimit, "rate-limit", "rl", 0, "maximum number of http requests to send per second (global)"),
-		flagSet.RateLimitMapVarP(&options.RateLimits, "rate-limits", "rls", defaultRateLimits, "maximum number of http requests to send per second for providers in key=value format (-rls hackertarget=10/m)", goflags.NormalizedStringSliceOptions),
+		flagSet.RateLimitMapVarP(&options.RateLimits, "rate-limits", "rls", defaultRateLimits, "maximum number of http requests to send per second for providers in key=value format (-rls hackertarget=20/m)", goflags.NormalizedStringSliceOptions),
 		flagSet.IntVar(&options.Threads, "t", 20, "number of concurrent goroutines for resolving (-active only)"),
 	)
 
